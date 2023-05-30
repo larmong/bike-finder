51e8b72aaae0e128cfd1f16732e295ec0caeee88
larmong <larmong@naver.com>
2023-05-31 05:25:14 +0900

    [ADD] board01

--- src/components/commons/boards/board01/Board01.presenter.tsx
+++ src/components/commons/boards/board01/Board01.presenter.tsx
@@ -1,3 +1,60 @@
+import * as S from "./Board01.style";
+import { MdArrowForwardIos, MdQuestionAnswer } from "react-icons/md";
+import {
+  BoardBodyWrapper,
+  BoardCont,
+  BoardContText,
+  BoardIcon,
+} from "./Board01.style";
+
 export default function Board01UI() {
-  return <>게시판</>;
+  return (
+    <S.Board>
+      <S.BoardHead>
+        <S.BoardItem>제목</S.BoardItem>
+      </S.BoardHead>
+      <S.BoardBodyWrapper>
+        <S.BoardBody>
+          <S.BoardItem>
+            삼성에서 자전거 3,000대, 대여소 300개소 등을 기부하였습니다.
+          </S.BoardItem>
+          <S.BoardIcon>
+            <MdArrowForwardIos />
+          </S.BoardIcon>
+          <S.BoardCont>
+            <MdQuestionAnswer />
+            <S.BoardContText>
+              <span>도움주신분</span> 삼성 <br />
+              <span>날짜</span> 2016-06-21 <br />
+              <br />
+              2016년 서울시 공공자전거 따릉이 확대구축 사업을 위해 자전거
+              3,000대, 대여소 300개소 등을 기부하였습니다.
+            </S.BoardContText>
+          </S.BoardCont>
+        </S.BoardBody>
+        <S.BoardBody>
+          <S.BoardItem>
+            (주)SK플래닛에서 자전거 100대를 기부하였습니다.
+          </S.BoardItem>
+          <S.BoardIcon>
+            <MdArrowForwardIos />
+          </S.BoardIcon>
+        </S.BoardBody>
+        <S.BoardBody>
+          <S.BoardItem>
+            (주)알톤스포츠에서 자전거 100대를 기부하였습니다.
+          </S.BoardItem>
+          <S.BoardIcon>
+            <MdArrowForwardIos />
+          </S.BoardIcon>
+        </S.BoardBody>
+        <S.BoardBody>
+          <S.BoardItem>우리은행에서 자전거를 기부하였습니다.</S.BoardItem>
+          <S.BoardIcon>
+            <MdArrowForwardIos />
+          </S.BoardIcon>
+        </S.BoardBody>
+      </S.BoardBodyWrapper>
+    </S.Board>
+  );
 }
---
+++ src/components/commons/boards/board01/Board01.style.ts
@@ -0,0 +1,70 @@
+import styled from "@emotion/styled";
+
+export const Board = styled.div``;
+export const BoardHead = styled.div`
+  display: grid;
+  grid-template-rows: 58px;
+  background: #f9f9f9;
+
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+  align-items: center;
+  text-align: center;
+
+  border-top: 1px solid #999999;
+  border-bottom: 1px solid #999999;
+`;
+export const BoardBodyWrapper = styled.div``;
+export const BoardBody = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 96px;
+  grid-template-rows: 50px;
+  border-bottom: 1px solid #e9e9e9;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+`;
+export const BoardIcon = styled.div`
+  cursor: pointer;
+  padding: 0 40px;
+  display: inline-block;
+  text-align: center;
+  svg {
+    rotate: -90deg;
+  }
+`;
+export const BoardItem = styled.span`
+  padding: 0 40px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const BoardCont = styled.div`
+  grid-column: 1 / 3;
+  padding: 30px 20px;
+  background: #fafafa;
+  display: flex;
+  border-top: 1px solid #e9e9e9;
+  svg {
+    width: 30px;
+    height: 30px;
+    color: #0d8f68;
+  }
+`;
+export const BoardContText = styled.div`
+  background: #fafafa;
+  margin-left: 10px;
+  margin-top: 2px;
+  font-size: 16px;
+  line-height: 30px;
+  color: #777;
+  span {
+    display: inline-block;
+    font-weight: 700;
+    color: #333;
+  }
+`;
