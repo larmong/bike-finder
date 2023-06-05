69a7b9dc6635efa6be8e820ac9c55bf168193b86
larmong <larmong@naver.com>
2023-06-05 17:34:55 +0900

    [ADD] citizen - info(react-player)

--- src/components/units/citizen/info/Info.container.tsx
+++ src/components/units/citizen/info/Info.container.tsx
@@ -1,3 +1,26 @@
+import { InfoGroup, InfoTitle, Wrapper, Youtube } from "./Info.style";
+
 export default function Info() {
-  return <>Info</>;
+  return (
+    <Wrapper>
+      <InfoGroup>
+        <InfoTitle>대여방법</InfoTitle>
+        <Youtube
+          width="100%"
+          height="600"
+          url="https://www.youtube.com/watch?v=2i2FpMWQBnU"
+          rel={0}
+        ></Youtube>
+      </InfoGroup>
+      <InfoGroup>
+        <InfoTitle>반납방법</InfoTitle>
+        <Youtube
+          width="100%"
+          height="600"
+          url="https://www.youtube.com/watch?v=qV8lem9vy8U"
+          rel={0}
+        ></Youtube>
+      </InfoGroup>
+    </Wrapper>
+  );
 }
---
+++ src/components/units/citizen/info/Info.style.ts
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import ReactPlayer from "react-player";
+
+export const Wrapper = styled.div`
+  margin-top: 70px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+export const InfoGroup = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const InfoTitle = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 30px;
+  color: #444444;
+`;
+
+export const Youtube = styled(ReactPlayer)`
+  width: 100%;
+  height: 600px;
+`;
