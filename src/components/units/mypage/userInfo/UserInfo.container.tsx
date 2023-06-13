b5f51641962fcd5474d1616de844ad85782eb8bd
larmong <larmong@naver.com>
2023-06-14 00:50:49 +0900

    [ADD] userInfo - default

--- src/components/units/mypage/userInfo/UserInfo.container.tsx
+++ src/components/units/mypage/userInfo/UserInfo.container.tsx
@@ -4,6 +4,9 @@ import UserInfoCard from "./card/UserInfoCard.container";
 import UserInfoFaq from "./faq/UserInfoFaq.container";
 import RentalPw from "./rentalPw/RentalPw.container";
 import Secession from "./secession/Secession.container";
+import Input01 from "../../../commons/inputs/input/input01/Input01.container";
+import { useState } from "react";
+import Button01 from "../../../commons/buttons/button01/Button01.container";
 
 export default function UserInfo(props) {
   const TAB_MENUS = [
@@ -29,20 +32,71 @@ export default function UserInfo(props) {
     },
   ];
 
+  const [userPw, setUserPw] = useState<number | undefined>(undefined);
+  const [userPwCheck, setUserPwCheck] = useState<number | undefined>(undefined);
+
+  const onChangeUserPw = (event) => {
+    setUserPw(event.target.value);
+  };
+  const onChangeUserPwCheck = (event) => {
+    setUserPwCheck(event.target.value);
+  };
+
+  const onClickButton = () => {
+    // 개인정보 저장
+  };
+
   return (
     <S.Wrapper>
       <Tab01 TAB_MENUS={TAB_MENUS} tabWidth="140px" />
-      {props.tab === "card" ? (
-        <UserInfoCard />
-      ) : props.tab === "rental" ? (
-        <RentalPw />
-      ) : props.tab === "faq" ? (
-        <UserInfoFaq />
-      ) : props.tab === "secession" ? (
-        <Secession />
-      ) : (
-        <div>개인정보</div>
-      )}
+      <S.Contents>
+        {props.tab === "card" ? (
+          <UserInfoCard />
+        ) : props.tab === "rental" ? (
+          <RentalPw />
+        ) : props.tab === "faq" ? (
+          <UserInfoFaq />
+        ) : props.tab === "secession" ? (
+          <Secession />
+        ) : (
+          <S.UserInfo>
+            <S.Table01>
+              <S.TableItem01 className="t-head">아이디</S.TableItem01>
+              <S.TableItem01>larmong</S.TableItem01>
+              <S.TableItem01 className="t-head">비밀번호</S.TableItem01>
+              <S.TableItem01>
+                <Input01
+                  inputType="password"
+                  onChangeValue={onChangeUserPw}
+                  valueData={setUserPw}
+                />
+              </S.TableItem01>
+              <S.TableItem01 className="t-head">비밀번호 확인</S.TableItem01>
+              <S.TableItem01>
+                <Input01
+                  inputType="password"
+                  onChangeValue={onChangeUserPwCheck}
+                  valueData={setUserPwCheck}
+                />
+              </S.TableItem01>
+              {/* 이메일주소, 휴대폰번호, 주소 등 변경 가능하도록 인풋으로 변경하고 기본 value 값은 user 정보를 불러옴 */}
+              <S.TableItem01 className="t-head">이메일 주소</S.TableItem01>
+              <S.TableItem01>larmong@naver.com</S.TableItem01>
+              <S.TableItem01 className="t-head">휴대폰 번호</S.TableItem01>
+              <S.TableItem01>010-1234-5678</S.TableItem01>
+              <S.TableItem01 className="t-head">주소</S.TableItem01>
+              <S.TableItem01>서울시 강남구 어쩌구 저쩌구</S.TableItem01>
+              <S.TableItem01 className="t-head">생년월일</S.TableItem01>
+              <S.TableItem01>1999-11-11</S.TableItem01>
+            </S.Table01>
+            <Button01
+              onClickButton={onClickButton}
+              btnWidth="200px"
+              btnText="저장하기"
+            />
+          </S.UserInfo>
+        )}
+      </S.Contents>
     </S.Wrapper>
   );
 }
--- src/components/units/mypage/userInfo/UserInfo.style.ts
+++ src/components/units/mypage/userInfo/UserInfo.style.ts
@@ -1,4 +1,36 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div``;
-export const Contents = styled.div``;
+export const Contents = styled.div`
+  margin-top: 40px;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+// Table01
+export const Table01 = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 225px 1fr;
+  font-size: 16px;
+  line-height: 18px;
+  letter-spacing: -0.05em;
+  border-top: 1px solid #666666;
+  border-bottom: 1px solid #666666;
+`;
+export const TableItem01 = styled.p`
+  height: 70px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #e9e9e9;
+  &.t-head {
+    justify-content: center;
+    font-weight: 700;
+    background: #f9f9f9;
+  }
+`;
