e065b660b0bb3596101349cc72a891bcfc8c780c
larmong <larmong@naver.com>
2023-06-20 02:44:11 +0900

    [FIX] kakao map - user 현재위치 가져오기

--- src/components/units/station/Station.container.tsx
+++ src/components/units/station/Station.container.tsx
@@ -1,54 +1,51 @@
 import * as S from "./Station.tyles";
 import Title02 from "../../commons/titles/title02/Title02.container";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 declare const window: typeof globalThis & {
   kakao: any;
 };
 
 export default function Station() {
-  const [location, setLocation] = useState([]);
-  const [isMapLoaded, setIsMapLoaded] = useState(false);
-
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
       "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=cac58bf014bb232f01fb4c8b9d4357c3";
     document.head.appendChild(script);
 
-    if ("geolocation" in navigator) {
-      /* 위치정보 사용 가능 */
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation([position.coords.latitude, position.coords.longitude]);
-      });
-    } else {
-      /* 위치정보 사용 불가능 */
-      setLocation([37.556944, 126.923855]);
-    }
-
     script.onload = () => {
-      if (location.length > 0 && !isMapLoaded) {
-        setIsMapLoaded(true);
+      window.kakao.maps.load(function () {
+        if ("geolocation" in navigator) {
+          // 위치정보 사용 가능
+          navigator.geolocation.getCurrentPosition((position) => {
+            getMap(position.coords.latitude, position.coords.longitude);
+          });
+        } else {
+          // 위치정보 사용 불가능
+          getMap(37.556944, 126.923855);
+        }
 
-        window.kakao.maps.load(() => {
+        const getMap = (latitude: number, longitude: number) => {
           const container = document.getElementById("map");
           const options = {
-            center: new window.kakao.maps.LatLng(location[0], location[1]),
+            center: new window.kakao.maps.LatLng(latitude, longitude),
             level: 3,
           };
 
           const map = new window.kakao.maps.Map(container, options);
-        });
-      }
+        };
+      });
     };
-  }, [location, isMapLoaded]);
+  }, []);
 
   return (
     <S.Wrapper>
       <S.Container>
         <Title02 title="대여소 조회" />
         <S.Contents>
-          <S.Map id="map"></S.Map>
+          <S.Map id="map">
+            <S.MapLoading>지도를 로딩중입니다...</S.MapLoading>
+          </S.Map>
         </S.Contents>
       </S.Container>
     </S.Wrapper>
--- src/components/units/station/Station.tyles.ts
+++ src/components/units/station/Station.tyles.ts
@@ -15,3 +15,13 @@ export const Map = styled.div`
   width: 100%;
   height: 800px;
 `;
+
+export const MapLoading = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #eeeeee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666666;
+`;
