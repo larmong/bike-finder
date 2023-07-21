cf4c71a19bc0fe80b3f9adc86e9905a36f7c6dfd
larmong <larmong@naver.com>
2023-07-21 21:16:58 +0900

    [FIX] pwInquiry

--- src/components/units/pwInquiry/PwInquiry.container.tsx
+++ src/components/units/pwInquiry/PwInquiry.container.tsx
@@ -7,19 +7,28 @@ import { BtnGroup, Icon, Message } from "../join/joinSuccess/JoinSuccess.style";
 import Title02 from "../../commons/titles/title02/Title02.container";
 import Input01 from "../../commons/inputs/input/input01/Input01.container";
 import Button01 from "../../commons/buttons/button01/Button01.container";
+import {
+  InputGroup,
+  InputItem,
+  InputTitle,
+} from "../join/userAuth/UserAuth.style";
+import Input03 from "../../commons/inputs/input/input03/input03.container";
+import Input02 from "../../commons/inputs/input/input02/Input02.container";
+import Button03 from "../../commons/buttons/button03/button03.container";
+import TimerContainer from "../../commons/utils/timer/Timer.container";
+import { CustomChangeEvent } from "../../../commons/types/global.types";
 
 export default function PwInquiry() {
   const router = useRouter();
 
-  const [userName, setUserName] = useState<number | undefined>(undefined);
-  const [userBirth, setUserBirth] = useState<number | undefined>(undefined);
+  const [userInfo, setUserInfo] = useState({
+    userId: "",
+    birth: "",
+    phone: "",
+    authNum: "",
+  });
 
-  const onChangeUserName = (event) => {
-    setUserName(event.target.value);
-  };
-  const onChangeUserBirth = (event) => {
-    setUserBirth(event.target.value);
-  };
+  const [authNumState, setAuthNumState] = useState<boolean>(false);
 
   const [userAuth, setUserAuth] = useState(false);
   const [userPwChange, setUserPwChange] = useState(false);
@@ -31,10 +40,41 @@ export default function PwInquiry() {
   };
 
   const onClickMoveToMain = () => {
-    router.push("/");
+    void router.push("/");
   };
   const onClickMoveToLogin = () => {
-    router.push("/login");
+    void router.push("/login");
+  };
+
+  const onChangeUserName = (event: CustomChangeEvent) => {
+    setUserInfo({
+      ...userInfo,
+      [event.target.id]: event.target.value,
+    });
+  };
+  const onChangeUserInfo = (value: string, id: string) => {
+    setUserInfo({
+      ...userInfo,
+      [id]: value,
+    });
+  };
+  const onClickAuthNumBtn = () => {
+    if (String(userInfo.phone).length === 11) {
+      setAuthNumState(true);
+    } else {
+      alert("핸드폰 번호를 확인해주세요.");
+    }
+  };
+
+  const [userNewPw, setUserNewPw] = useState({
+    pw: "",
+    pw2: "",
+  });
+  const onChangeUserPw = (event: CustomChangeEvent) => {
+    setUserNewPw({
+      ...userNewPw,
+      [event.target.id]: event.target.value,
+    });
   };
 
   return (
@@ -43,32 +83,63 @@ export default function PwInquiry() {
         <Title02 title="비밀번호 변경" />
         {!userAuth && !userPwChange ? (
           <S.Contents>
-            <S.InputGroup>
-              <S.InputItem>
-                <S.InputTitle>아&nbsp;&nbsp;이&nbsp;&nbsp;디</S.InputTitle>
+            <InputGroup>
+              <InputItem>
+                <InputTitle>아&nbsp;&nbsp;이&nbsp;&nbsp;디</InputTitle>
                 <Input01
                   inputType="text"
                   onChangeValue={onChangeUserName}
-                  valueData={userName}
+                  valueData={userInfo.userId}
+                  inputId="name"
+                  inputClass="bottomBorder"
+                  placeholderData="아이디를 입력해주세요."
                 />
-              </S.InputItem>
-              <S.InputItem>
-                <S.InputTitle>휴&nbsp;&nbsp;대&nbsp;&nbsp;폰</S.InputTitle>
-                <Input01
-                  inputType="text"
-                  onChangeValue={onChangeUserBirth}
-                  valueData={userBirth}
+              </InputItem>
+              <InputItem>
+                <InputTitle>생년월일</InputTitle>
+                <Input03
+                  inputId="birth"
+                  inputMaxLength={8}
+                  onChangeNumber={onChangeUserInfo}
+                  valueData={userInfo.birth}
+                  placeholderData="생년월일 8자리를 입력해주세요. (ex.19991010)"
                 />
-              </S.InputItem>
-              <S.InputItem>
-                <S.InputTitle>인증번호</S.InputTitle>
-                <Input01
-                  inputType="text"
-                  onChangeValue={onChangeUserBirth}
-                  valueData={userBirth}
+              </InputItem>
+              <InputItem>
+                <InputTitle>휴&nbsp;&nbsp;대&nbsp;&nbsp;폰</InputTitle>
+                <Input02
+                  inputId="phone"
+                  onChangePhone={onChangeUserInfo}
+                  valueData={userInfo.phone}
                 />
-              </S.InputItem>
-            </S.InputGroup>
+                {authNumState ? (
+                  <Button03
+                    onClickButton={onClickAuthNumBtn}
+                    btnText="재전송"
+                  />
+                ) : (
+                  <Button03
+                    onClickButton={onClickAuthNumBtn}
+                    btnText="인증번호전송"
+                  />
+                )}
+              </InputItem>
+              {authNumState ? (
+                <InputItem>
+                  <InputTitle>인증번호</InputTitle>
+                  <Input03
+                    inputId="authNum"
+                    inputMaxLength={6}
+                    onChangeNumber={onChangeUserInfo}
+                    valueData={userInfo.authNum}
+                    placeholderData="인증번호 6자리를 입력해주세요."
+                  />
+                  <TimerContainer />
+                </InputItem>
+              ) : (
+                ""
+              )}
+            </InputGroup>
             <Button01
               onClickButton={onClickMoveToSuccess}
               btnWidth="200px"
@@ -81,28 +152,30 @@ export default function PwInquiry() {
               <MdCheckCircle />
             </S.Icon>
             <S.Message>변경하실 비밀번호를 입력해 주세요.</S.Message>
-            <S.InputGroup>
-              <S.InputItem>
-                <S.InputTitle>비밀번호</S.InputTitle>
+            <InputGroup>
+              <InputItem>
+                <InputTitle>비밀번호</InputTitle>
                 <Input01
-                  inputType="text"
-                  onChangeValue={onChangeUserBirth}
-                  valueData={userBirth}
+                  inputId="pw"
+                  inputType="password"
+                  onChangeValue={onChangeUserPw}
+                  valueData={userNewPw.pw}
                 />
-              </S.InputItem>
-              <S.InputItem>
-                <S.InputTitle>
+              </InputItem>
+              <InputItem>
+                <InputTitle>
                   비밀번호
                   <br />
                   확인
-                </S.InputTitle>
+                </InputTitle>
                 <Input01
-                  inputType="text"
-                  onChangeValue={onChangeUserBirth}
-                  valueData={userBirth}
+                  inputId="pw2"
+                  inputType="password"
+                  onChangeValue={onChangeUserPw}
+                  valueData={userNewPw.pw2}
                 />
-              </S.InputItem>
-            </S.InputGroup>
+              </InputItem>
+            </InputGroup>
             <S.BtnGroup>
               <Button01
                 onClickButton={onClickMoveToPwChange}
