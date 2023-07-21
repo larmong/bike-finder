87c15d3dcf149bc9153d524a7edcd31faed636a7
larmong <larmong@naver.com>
2023-07-21 15:30:52 +0900

    [DESIGN] mobile - ranking

--- src/components/units/citizen/ranking/Ranking.style.ts
+++ src/components/units/citizen/ranking/Ranking.style.ts
@@ -6,22 +6,6 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 50px;
-  ${mediaQuery[2]} {
-  }
-`;
-
-export const Notice = styled.div`
-  margin-top: -1px;
-  padding: 30px 20px;
-  border: 1px solid #e9e9e9;
-  font-size: 14px;
-  line-height: 23px;
-  letter-spacing: -0.07em;
-  strong {
-    display: block;
-    font-weight: 700;
-    color: #dc1919;
-  }
 `;
 
 export const RankingWrapper = styled.div`
@@ -46,10 +30,13 @@ export const UserRankingTable = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: 185px 1fr 185px;
-
   font-size: 14px;
   line-height: 16px;
   letter-spacing: -0.05em;
+  ${mediaQuery[2]} {
+    grid-template-columns: 1fr 1fr 1fr;
+    font-size: 13px;
+  }
 `;
 export const TableItem01 = styled.p`
   padding: 14px;
@@ -57,7 +44,6 @@ export const TableItem01 = styled.p`
   text-align: center;
   align-items: center;
   justify-content: center;
-
   border-bottom: 1px solid #e9e9e9;
 
   &.t-head {
