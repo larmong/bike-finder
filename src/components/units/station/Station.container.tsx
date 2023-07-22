636cc00ece3fea9d4287d4ce2faeae155d9de20e
larmong <larmong@naver.com>
2023-07-22 19:07:05 +0900

    [FIX] map - bike station 연동, marker 추가

--- src/components/units/station/Station.container.tsx
+++ src/components/units/station/Station.container.tsx
@@ -1,5 +1,6 @@
 import * as S from "./Station.style";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Container } from "../../../commons/style/global.style";
 import Title02 from "../../commons/titles/title02/Title02.container";
 
@@ -8,35 +9,86 @@ declare const window: typeof globalThis & {
 };
 
 export default function Station() {
+  const [bikeStations, setBikeStations] = useState<any[]>([]);
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
+    if ("geolocation" in navigator) {
+      // 위치정보 사용 가능
+      navigator.geolocation.getCurrentPosition((position) => {
+        setPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      // 위치정보 사용 불가능
+      setPosition({
+        latitude: 37.556944,
+        longitude: 126.923855,
+      });
+    }
+
     const script = document.createElement("script");
     script.src =
       "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=cac58bf014bb232f01fb4c8b9d4357c3";
     document.head.appendChild(script);
 
     script.onload = () => {
-      window.kakao.maps.load(function () {
-        if ("geolocation" in navigator) {
-          // 위치정보 사용 가능
-          navigator.geolocation.getCurrentPosition((position) => {
-            getMap(position.coords.latitude, position.coords.longitude);
-          });
-        } else {
-          // 위치정보 사용 불가능
-          getMap(37.556944, 126.923855);
-        }
-
-        const getMap = (latitude: number, longitude: number) => {
-          const container = document.getElementById("map");
-          const options = {
-            center: new window.kakao.maps.LatLng(latitude, longitude),
-            level: 3,
-          };
-
-          const map = new window.kakao.maps.Map(container, options);
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(
+            position.latitude,
+            position.longitude
+          ),
+          level: 4,
         };
+        const map = new window.kakao.maps.Map(container, options);
+        setIsLoading(false);
+
+        // 데이터를 받아온 후에 마커를 그리도록 이동
+        if (bikeStations.length > 0) {
+          bikeStations.forEach((station: any) => {
+            const latitude = Number(station.stationLatitude);
+            const longitude = Number(station.stationLongitude);
+            const bikeImage = new window.kakao.maps.MarkerImage(
+              "https://firebasestorage.googleapis.com/v0/b/bike-finder-1121a.appspot.com/o/bikefinder%2Fmarker.svg?alt=media&token=b255d980-98f0-45c1-aff4-0d4f0f367af7",
+              new window.kakao.maps.Size(40, 40),
+              {
+                offset: new window.kakao.maps.Point(20, 40),
+              }
+            );
+            const markerPosition = new window.kakao.maps.LatLng(
+              latitude,
+              longitude
+            );
+            const marker = new window.kakao.maps.Marker({
+              position: markerPosition,
+              image: bikeImage,
+            });
+            marker.setMap(map);
+          });
+        }
       });
     };
+  }, [bikeStations, position]); // bikeStations와 position 변수를 의존성 배열에 추가
+
+  useEffect(() => {
+    const apiUrl =
+      "http://openapi.seoul.go.kr:8088/77447a58706c617237364d6a694774/json/bikeList/1/1000/";
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const bikeStations = response.data?.rentBikeStatus?.row;
+        if (bikeStations) {
+          setBikeStations(bikeStations);
+        }
+      })
+      .catch((error) => {
+        console.error("따릉이 정류장 데이터를 가져오는 중 에러 발생:", error);
+      });
   }, []);
 
   return (
@@ -45,7 +97,9 @@ export default function Station() {
         <Title02 title="대여소 조회" />
         <S.Contents>
           <S.Map id="map">
-            <S.MapLoading>지도를 로딩중입니다...</S.MapLoading>
+            {isLoading ? (
+              <S.MapLoading>지도를 로딩중입니다...</S.MapLoading>
+            ) : null}
           </S.Map>
         </S.Contents>
       </Container>
