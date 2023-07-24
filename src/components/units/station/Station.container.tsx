39929051c3a3bc2a7cc9168c8f41638cfeaab02f
larmong <larmong@naver.com>
2023-07-25 02:52:44 +0900

    [FIX] station 로직 수정

--- src/components/units/station/Station.container.tsx
+++ src/components/units/station/Station.container.tsx
@@ -11,71 +11,91 @@ declare const window: typeof globalThis & {
 };
 
 export default function Station() {
-  const { data } = useQuery<allStations>(FETCH_BIKE_STATIONS);
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+    isAvailable: false,
+  });
+  const { loading, error, data } = useQuery<allStations>(FETCH_BIKE_STATIONS);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       // 위치정보 사용 가능
-      navigator.geolocation.getCurrentPosition((position) => {
-        setPosition({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setPosition({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            isAvailable: true,
+          });
+        },
+        () => {
+          // 위치정보 사용 불가능
+          setPosition({
+            latitude: 37.5556488,
+            longitude: 126.91062927,
+            isAvailable: false,
+          });
+        }
+      );
     } else {
       // 위치정보 사용 불가능
       setPosition({
-        latitude: 37.556944,
-        longitude: 126.923855,
+        latitude: 37.5556488,
+        longitude: 126.91062927,
+        isAvailable: false,
       });
     }
+  }, []);
 
-    const script = document.createElement("script");
-    script.src =
-      "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=cac58bf014bb232f01fb4c8b9d4357c3";
-    document.head.appendChild(script);
+  useEffect(() => {
+    if (!loading && !error && position.isAvailable) {
+      const script = document.createElement("script");
+      script.src =
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=cac58bf014bb232f01fb4c8b9d4357c3";
+      document.head.appendChild(script);
 
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById("map");
-        const options = {
-          center: new window.kakao.maps.LatLng(
-            position.latitude,
-            position.longitude
-          ),
-          level: 4,
-        };
-        const map = new window.kakao.maps.Map(container, options);
-        setIsLoading(false);
+      script.onload = () => {
+        window.kakao.maps.load(() => {
+          const container = document.getElementById("map");
+          const options = {
+            center: new window.kakao.maps.LatLng(
+              position.latitude,
+              position.longitude
+            ),
+            level: 4,
+          };
+          const map = new window.kakao.maps.Map(container, options);
+          setIsLoading(false);
 
-        const bikeStations = data?.allStations || [];
-        if (bikeStations.length > 0) {
-          bikeStations.forEach((station: any) => {
-            const latitude = Number(station.stationLatitude);
-            const longitude = Number(station.stationLongitude);
-            const bikeImage = new window.kakao.maps.MarkerImage(
-              "https://firebasestorage.googleapis.com/v0/b/bike-finder-1121a.appspot.com/o/bikefinder%2Fstation%2Fmarker.svg?alt=media&token=cadd0535-382e-47c8-acaf-c15e75108b63",
-              new window.kakao.maps.Size(40, 40),
-              {
-                offset: new window.kakao.maps.Point(20, 40),
-              }
-            );
-            const markerPosition = new window.kakao.maps.LatLng(
-              latitude,
-              longitude
-            );
-            const marker = new window.kakao.maps.Marker({
-              position: markerPosition,
-              image: bikeImage,
+          const bikeStations = data?.allStations || [];
+          if (bikeStations.length > 0) {
+            bikeStations.forEach((station: any) => {
+              const latitude = Number(station.stationLatitude);
+              const longitude = Number(station.stationLongitude);
+              const bikeImage = new window.kakao.maps.MarkerImage(
+                "https://firebasestorage.googleapis.com/v0/b/bike-finder-1121a.appspot.com/o/bikefinder%2Fstation%2Fmarker.svg?alt=media&token=cadd0535-382e-47c8-acaf-c15e75108b63",
+                new window.kakao.maps.Size(40, 40),
+                {
+                  offset: new window.kakao.maps.Point(20, 40),
+                }
+              );
+              const markerPosition = new window.kakao.maps.LatLng(
+                latitude,
+                longitude
+              );
+              const marker = new window.kakao.maps.Marker({
+                position: markerPosition,
+                image: bikeImage,
+              });
+              marker.setMap(map);
             });
-            marker.setMap(map);
-          });
-        }
-      });
-    };
-  }, [data, position]);
+          }
+        });
+      };
+    }
+  }, [data, loading, error, position]);
 
   return (
     <S.Wrapper>
