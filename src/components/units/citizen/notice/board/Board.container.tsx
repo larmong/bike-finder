85e0029cb0d7c177a3330781c64ac772cf948cbc
larmong <larmong@naver.com>
2023-07-25 16:11:13 +0900

    [FIX] notice board mobile UX 개선

--- src/components/units/citizen/notice/board/Board.container.tsx
+++ src/components/units/citizen/notice/board/Board.container.tsx
@@ -42,42 +42,56 @@ export default function NoticeBoard(props: IPropsNoticeBoard) {
   }, [props.boardData]);
 
   return (
-    <S.Board>
-      <Search01 handleSearch={handleSearch} />
-      <S.BoardHead isColumns={BOARD_DETAIL.columns}>
-        {BOARD_DETAIL.title.map((el: IBoardDetailTitleType, index: number) => (
-          <S.BoardItem key={index}>{el}</S.BoardItem>
-        ))}
-      </S.BoardHead>
-      <S.BoardBody>
-        {paginatedData?.map((el: any) =>
-          el.top ? (
-            <S.BoardItemWrapper key={el.id} isColumns={BOARD_DETAIL.columns}>
-              <S.NoticeBoardItem className="board-item-left">
-                <em>공지</em>
-                <span id={el.id} onClick={props.onClickBoardDetail}>
-                  {el.title}
-                </span>
-              </S.NoticeBoardItem>
-              <S.NoticeBoardItem>{el.date}</S.NoticeBoardItem>
-            </S.BoardItemWrapper>
-          ) : (
-            <S.BoardItemWrapper key={el.id} isColumns={BOARD_DETAIL.columns}>
-              <S.BoardItem className="board-item-left">
-                <span id={el.id} onClick={props.onClickBoardDetail}>
-                  {el.title}
-                </span>
-              </S.BoardItem>
-              <S.BoardItem>{el.date}</S.BoardItem>
-            </S.BoardItemWrapper>
-          )
-        )}
-      </S.BoardBody>
-      <Pagination01
-        noticeLength={noticeLength}
-        pageSize={pageSize}
-        handlePageChange={handlePageChange}
-      />
-    </S.Board>
+    <S.Wrapper>
+      <S.BoardWrapper>
+        <Search01 handleSearch={handleSearch} />
+        <S.BoardContainer>
+          <S.Board widthValue="auto">
+            <S.BoardHead isColumns={BOARD_DETAIL.columns}>
+              {BOARD_DETAIL.title.map(
+                (el: IBoardDetailTitleType, index: number) => (
+                  <S.BoardItem key={index}>{el}</S.BoardItem>
+                )
+              )}
+            </S.BoardHead>
+            <S.BoardBody>
+              {paginatedData?.map((el: any) =>
+                el.top ? (
+                  <S.BoardItemWrapper
+                    key={el.id}
+                    isColumns={BOARD_DETAIL.columns}
+                  >
+                    <S.NoticeBoardItem className="board-item-left">
+                      <em>공지</em>
+                      <span id={el.id} onClick={props.onClickBoardDetail}>
+                        {el.title}
+                      </span>
+                    </S.NoticeBoardItem>
+                    <S.NoticeBoardItem>{el.date}</S.NoticeBoardItem>
+                  </S.BoardItemWrapper>
+                ) : (
+                  <S.BoardItemWrapper
+                    key={el.id}
+                    isColumns={BOARD_DETAIL.columns}
+                  >
+                    <S.BoardItem className="board-item-left">
+                      <span id={el.id} onClick={props.onClickBoardDetail}>
+                        {el.title}
+                      </span>
+                    </S.BoardItem>
+                    <S.BoardItem>{el.date}</S.BoardItem>
+                  </S.BoardItemWrapper>
+                )
+              )}
+            </S.BoardBody>
+          </S.Board>
+        </S.BoardContainer>
+        <Pagination01
+          noticeLength={noticeLength}
+          pageSize={pageSize}
+          handlePageChange={handlePageChange}
+        />
+      </S.BoardWrapper>
+    </S.Wrapper>
   );
 }
